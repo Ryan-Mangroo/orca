2b33cfd9a6608dd5c91efc375e6c92b0fc2b18e1
Jesse Williams <jess@workwoo.com>
2016-10-05 22:03:55 -0700

    Fixed several things on mobile view

--- public/js/utils.js
+++ public/js/utils.js
@@ -13,3 +13,16 @@ var log = {
 		console.log('##### END OBJECT');
 	},
 };
+
+// Makes navbars collapse when links are clicked
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+$(document).click(function(e) {
+    if (e.target.id == 'mainLink' || !$(e.target).is('a')) {
+        $('.collapse').collapse('hide');        
+    }
+});
